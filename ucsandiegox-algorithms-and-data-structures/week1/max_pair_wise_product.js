f95000f4ef1bc7cedb6d924e8163cdf34c77700a
MichelML <michmoreau.l@gmail.com>
2018-02-04 18:08:10 -0500

    reimplement algo based on one input

--- ucsandiegox-algorithms-and-data-structures/week1/max_pair_wise_product.js
+++ ucsandiegox-algorithms-and-data-structures/week1/max_pair_wise_product.js
@@ -2,22 +2,13 @@ var readline = require('readline')
 
 var rl = readline.createInterface({input: process.stdin, terminal: false})
 
-var lineCount = 0
-var arrayLength = 2
-var sortedArrayOfPositiveIntegers = []
-
 rl.on('line', (line) => {
-  lineCount += 1
-  if (lineCount === 1) {
-    arrayLength = parseInt(line.trim(), 10)
-  } else if (lineCount === 2) {
-    sortedArrayOfPositiveIntegers = line.trim()
+  var sortedArrayOfPositiveIntegers = line.trim()
             .split(' ')
             .map(num => parseInt(num, 10))
             .sort((a, b) => a > b)
 
-    var arrLength = sortedArrayOfPositiveIntegers.length
-    console.log(sortedArrayOfPositiveIntegers[arrLength - 1] * sortedArrayOfPositiveIntegers[arrLength - 2])
-    process.exit()
-  }
+  var arrLength = sortedArrayOfPositiveIntegers.length
+  console.log(sortedArrayOfPositiveIntegers[arrLength - 1] * sortedArrayOfPositiveIntegers[arrLength - 2])
+  process.exit()
 })
