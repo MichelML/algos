5b2e3f3224ae9bc2559edc9398aad947c31cd409
MichelML <michmoreau.l@gmail.com>
2018-03-11 10:08:13 -0400

    rm parens

--- implementations/week5/primitive_calculator.py
+++ implementations/week5/primitive_calculator.py
@@ -8,13 +8,13 @@ def optimal_sequence(n):
             a[i] = min(1+a[i//2], a[i])
         if i % 3 == 0:
             a[i] = min(1+a[i//3], a[i])
-    while (n > 1):
+    while n > 1:
         sequence.append(n)
         if a[n-1] == a[n]-1:
             n = n-1
-        elif (n % 2 == 0 and (a[n//2] == a[n]-1)):
+        elif n % 2 == 0 and a[n//2] == a[n]-1:
             n = n//2
-        elif (n % 3 == 0 and (a[n//3] == a[n]-1)):
+        elif n % 3 == 0 and a[n//3] == a[n]-1:
             n = n//3
     sequence.append(1)
     return reversed(sequence)
