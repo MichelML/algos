2d974f44301592543f6971bbbd412d126cbb5930
MichelML <michmoreau.l@gmail.com>
2018-03-11 09:50:51 -0400

    implement dynamic programming sequence finder

--- implementations/week5/primitive_calculator.py
+++ implementations/week5/primitive_calculator.py
@@ -1,36 +1,27 @@
 # Uses python3
-import sys
-import functools
-
-def memoize(obj):
-    cache = obj.cache = {}
-
-    @functools.wraps(obj)
-    def memoizer(*args, **kwargs):
-        if args not in cache:
-            cache[args] = obj(*args, **kwargs)
-        return cache[args]
-    return memoizer
-
-@memoize
-def multiply_by_3(x):
-    return x * 3
-
-@memoize
-def multiply_by_2(x):
-    return x * 2
-
-@memoize
-def add_one(x):
-    return x * 1
-
 def optimal_sequence(n):
-    if n == 1:
-        return [1]
+    sequence = []
+    a = [0]*(n+1)
+    for i in range(1, len(a)):
+        a[i] = a[i-1] + 1
+        if i % 2 == 0:
+            a[i] = min(1+a[i//2], a[i])
+        if i % 3 == 0:
+            a[i] = min(1+a[i//3], a[i])
+    while (n > 1):
+        sequence.append(n)
+        if a[n-1] == a[n]-1:
+            n = n-1
+        elif (n % 2 == 0 and (a[n//2] == a[n]-1)):
+            n = n//2
+        elif (n % 3 == 0 and (a[n//3] == a[n]-1)):
+            n = n//3
+    sequence.append(1)
+    return reversed(sequence)
 
-# input = sys.stdin.read()
-# n = int(input)
-# sequence = list(optimal_sequence(n))
-# print(len(sequence) - 1)
-# for x in sequence:
-#     print(x, end=' ')
+
+n = int(input())
+sequence = list(dp_sequence(n))
+print(len(sequence) - 1)
+for x in sequence:
+    print(x, end=' ')
