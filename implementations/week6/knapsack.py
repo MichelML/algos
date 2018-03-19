1bdb14292b94bc246f4812b567a7297aacce25ae
MichelML <michmoreau.l@gmail.com>
2018-03-19 02:26:41 -0400

    implement week 6 algos

--- implementations/week6/knapsack.py
+++ implementations/week6/knapsack.py
@@ -28,4 +28,4 @@ def optimal_weight(W, w):
 if __name__ == '__main__':
     input = sys.stdin.read()
     W, n, *w = list(map(int, input.split()))
-    print(knapSack(W, w, 1, n))
+    print(knapSack(W, w, w, n))
